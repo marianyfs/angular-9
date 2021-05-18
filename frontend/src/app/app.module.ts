a1d2a40d5bc2e96c72d0b9b6c5e592dd7bd309b1
Mariany Ferreira da Silva <fsilvamariany@gmail.com>
2021-05-17 23:49:15 -0300

    feat: add nav in components template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +27,8 @@ import { FooterComponent } from './components/template/footer/footer.component';
     BrowserAnimationsModule,
 
     MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
